b70d7ed7af436dfaf0bde70b867a2cdf003986db
Anthony Simpson <ansi6622@colorado.edu>
2016-05-09 21:20:26 -0600

    seeded successfully

--- migrations/20160509162931_the_first_one.js
+++ migrations/20160509162931_the_first_one.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('pirates', function(table){
+  table.increments();
+  table.string('name');
+  table.string('poison');
+  table.string('accessory');
+  table.string('image_url');
+  table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('pirates');
 };
---
+++ seeds/pirate.js
@@ -0,0 +1,11 @@
+
+exports.seed = function(knex, Promise) {
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('pirates').del(),
+
+    knex('pirates').insert({name: 'Anne Bonney', poison: 'Rum', accessory: 'Scythe', image_url: 'http://2.bp.blogspot.com/-WSKMEi_MH5U/U-AdUnMNuvI/AAAAAAAABI8/fv0BXLICx8c/s1600/Anne_Bonny_color.jpg'}),
+    knex('pirates').insert({name: 'Le\' Chuck', poison: 'Voodoo Goo', accessory: 'Push Pins', image_url: 'https://pbs.twimg.com/profile_images/1866704249/monkey-island-don-t-fuck-with-lechuck_design_400x400.png'}),
+    knex('pirates').insert({name: 'Blackbeard', poison: 'Blood of his Enemies', accessory: 'Pistol', image_url: 'https://d13yacurqjgara.cloudfront.net/users/58309/screenshots/1105038/blackbeard-01.jpg'})
+    );
+  };
